80d0f35fef199c3d5a06f4050a2bd872b97e84ad
shurupyan <shurupyan@gmail.com>
2023-08-19 19:41:39 +0200

    Added templates and tests

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
 # Create your views here.
-def home_page_view(request):
-    return HttpResponse("Hello, World!")
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
