622d07f47a6a4b67d42fb80cb17c4ebd5cfbbf99
shurupyan <shurupyan@gmail.com>
2023-08-20 14:58:53 +0200

    Added posts css updated html templates updated Posts model

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Post(models.Model):
-    text = models.TextField()
+    title = models.CharField(max_length=200, default="Post Title")
+    author = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+        default=User.objects.get_by_natural_key(username="admin").pk,
+    )
+    body = models.TextField()
 
     def __str__(self):
-        return self.text[:50]
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post detail", kwargs={"pk": self.pk})
