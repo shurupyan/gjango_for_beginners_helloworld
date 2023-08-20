7dab0717f1fd7c1847a888e6eff5c085ab098e6f
shurupyan <shurupyan@gmail.com>
2023-08-20 13:03:05 +0200

    Added posts app

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,5 @@ from .views import HomePageView, AboutPageView
 
 urlpatterns = [
     path("about/", AboutPageView.as_view(), name="about"),
-    path("", HomePageView.as_view(), name="home"),
+    # path("", HomePageView.as_view(), name="home"),
 ]
