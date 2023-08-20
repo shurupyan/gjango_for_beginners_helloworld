b1f1f6494b7a3a2116b7dc26c80fb2e41301cbe3
shurupyan <shurupyan@gmail.com>
2023-08-20 13:41:38 +0200

    Added posts tests moved homepage tests to Posts app

--- pages/tests.py
+++ pages/tests.py
@@ -3,22 +3,6 @@ from django.urls import reverse
 
 
 # Create your tests here.
-class HomepageTests(SimpleTestCase):
-    def test_url_exists_at_correct_location(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
-
-    def test_url_available_by_name(self):
-        response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code, 200)
-
-    def test_template_name_correct(self):
-        response = self.client.get(reverse("home"))
-        self.assertTemplateUsed(response, "home.html")
-
-    def test_template_content_correct(self):
-        response = self.client.get(reverse("home"))
-        self.assertContains(response, "<h1>Homepage</h1>")
 
 
 class AboutpageTests(SimpleTestCase):
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
+from .models import Post
+
 
 # Create your tests here.
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text="This is a test!")
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "This is a test!")
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "<h1>Message board homepage</h1>")
+        self.assertContains(response, "This is a test!")
